b3bd27cab9c8e84739fbb90f78b5044a3d9139b2
Yvonne <ypeng.tw@gmail.com>
2015-10-22 15:45:48 -0700

    Add files

---
+++ app/src/main/java/com/epicodus/photogallery/PuppyActivity.java
@@ -0,0 +1,66 @@
+package com.epicodus.photogallery;
+
+import java.util.Locale;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PuppyActivity extends FragmentActivity {
+
+    PuppyPageAdaptor mPuppyPageAdaptor;
+
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puppy);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        // mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mPuppyPageAdaptor = new PuppyPageAdaptor(getSupportFragmentManager(), getApplicationContext());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mPuppyPageAdaptor);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_puppy, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/epicodus/photogallery/PuppyFragment.java
@@ -0,0 +1,36 @@
+package com.epicodus.photogallery;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by YHoP on 10/22/15.
+ */
+public class PuppyFragment extends android.support.v4.app.Fragment {
+
+    public static final String ImageIDKey = "imagekey";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view =inflater.inflate(R.layout.fragment_puppy, container, false);
+
+        Bundle bundle = getArguments();
+
+        if(bundle != null){
+            int imageId = bundle.getInt(ImageIDKey);
+            setValues(view, imageId);
+        }
+
+        return view;
+    }
+
+    private void setValues(View view, int imageId) {
+        ImageView imageView = (ImageView) view.findViewById(R.id.puppyImage);
+        imageView.setImageResource(imageId);
+    }
+}
